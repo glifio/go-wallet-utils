ff6c4bc755338cde4dd1b58e50c49a41e21547c3
Jim Pick <jim@jimpick.com>
2023-07-18 22:04:29 -0700

    Add NewEthWalletTransactor() and go.mod

---
+++ eth_wallet.go
@@ -0,0 +1,29 @@
+package walletutils
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+// NewEthWalletTransactor is a utility method to easily create transaction
+// options for use with an Ethereum wallet with an optional passphrase.
+func NewEthWalletTransactor(wallet accounts.Wallet, account *accounts.Account, passphrase string, chainID *big.Int) (*bind.TransactOpts, error) {
+	if chainID == nil {
+		return nil, bind.ErrNoChainID
+	}
+	return &bind.TransactOpts{
+		From: account.Address,
+		Signer: func(address common.Address, tx *types.Transaction) (*types.Transaction, error) {
+			if address != account.Address {
+				return nil, bind.ErrNotAuthorized
+			}
+			return wallet.SignTxWithPassphrase(*account, passphrase, tx, chainID)
+		},
+		Context: context.Background(),
+	}, nil
+}
