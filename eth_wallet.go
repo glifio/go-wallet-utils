85e21ff0c82104d01a20828fea3313dee56eb31b
Jim Pick <jim@jimpick.com>
2023-08-01 00:16:37 -0700

    Debug logging

--- eth_wallet.go
+++ eth_wallet.go
@@ -2,6 +2,7 @@ package walletutils
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts"
@@ -13,6 +14,7 @@ import (
 // NewEthWalletTransactor is a utility method to easily create transaction
 // options for use with an Ethereum wallet with an optional passphrase.
 func NewEthWalletTransactor(wallet accounts.Wallet, account *accounts.Account, passphrase string, chainID *big.Int) (*bind.TransactOpts, error) {
+	fmt.Println("Jim NewEthWalletTransactor")
 	if chainID == nil {
 		return nil, bind.ErrNoChainID
 	}
@@ -22,6 +24,8 @@ func NewEthWalletTransactor(wallet accounts.Wallet, account *accounts.Account, p
 			if address != account.Address {
 				return nil, bind.ErrNotAuthorized
 			}
+			fmt.Printf("Jim sign wallet %+v\n", wallet)
+			fmt.Printf("Jim sign account %+v\n", account)
 			return wallet.SignTxWithPassphrase(*account, passphrase, tx, chainID)
 		},
 		Context: context.Background(),
